2c8d1644313c7a8c9d02e24d86a4ee4ec45e206c
hyewonlee98 <hyewon98@gmail.com>
2017-12-02 18:10:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// TODO: Let's add text to our About Me.
+
+// TODO: We can also create a new div AND set its contents without modifying the HTML File.
+
+
+/*Let's make our nav buttons listen to clicks! 
+ *We are attaching an event listener for the "click"
+ *event to the element with the id "home". When the "click" 
+ *event occurs, we execute the callback (the function).
+ * */
+
+// Same thing using different JavaScript syntax
